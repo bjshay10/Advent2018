aef02ed92df2a161d6cc9ffaf720310047323d7c
bjshay10 <bj.shay@d51schools.org>
2018-12-01 08:57:46 -0700

    Day 1 clean

--- day1/day1_1.js
+++ day1/day1_1.js
@@ -8,15 +8,11 @@ var frequency = 0;
 lineReader.on('line', (line) => {
     var fUpDown = line.substring(0,1);
     var ammount = parseInt(line.substring(1,line.length));
-    // console.log(fUpDown);
+
     if (fUpDown == '+') {
-        // console.log(`Add ${ammount}`);
         frequency += parseInt(ammount);
-        // console.log(`Total: ${frequency}`);
     } else {
-        // console.log(`Minus ${ammount}`);
         frequency -= parseInt(ammount);
-        // console.log(`Total: ${frequency}`);
     }
     console.log(`The Frequency is: ${frequency}`);
 });
