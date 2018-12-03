58f579bd5f73a1b9656adce3594e4a2de1072d5d
bjshay10 <bj.shay@d51schools.org>
2018-12-03 10:45:04 -0700

    Day 3 even though I didn't find the answer

--- day3/day3_1.js
+++ day3/day3_1.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-// var contents = fs.readFileSync('./input.txt', 'utf8');
-var contents = fs.readFileSync('./input_test.txt', 'utf8');
+var contents = fs.readFileSync('./input.txt', 'utf8');
+// var contents = fs.readFileSync('./input_test.txt', 'utf8');
 
 var claims = contents.split('\r\n');
 var claimNum = 0;
@@ -10,25 +10,67 @@ var fromEdgeY = 0;
 var distX = 0;
 var distY = 0;
 
-var square = {
-    x: 0,
-    y: 0,
-    used: 0
+class Sheet {
+    constructor (width = 1000, height = 1000) {
+        this._grid = Array
+            .from({length: height})
+            .map(() => Array
+                .from({length: width})
+                .map(() => 0));
+    }
+
+    reserve (claim) {
+        for (let y = 0; y < claim.height; y++) {
+          for (let x = 0; x < claim.width; x++) {
+            this._grid[y + claim.top][x + claim.left] += 1;
+          }
+        }
+    }
+
+    get reservedSquareInches () {
+        return this._grid.reduce((a, b) => {
+          return a + b.filter((x) => x >= 2).length;
+        }, 0);
+    }
+}
+
+const fabric = (input, width = 1000, height = 1000) => {
+    const sheet = new Sheet(width, height);
+    console.log(input);
+    input
+        .split('\n')
+        .map((x) => {
+            const parts = x.match(/#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/);
+
+            return {
+                id: +parts[1],
+                top: +parts[3],
+                left: +parts[2],
+                width: +parts[4],
+                height: +parts[5],
+            };
+        })
+        .forEach((claim) => sheet.reserve(claim));
+
+    return sheet.reservedSquareInches;
 };
 
-claims.forEach((claim) => {
-    //breaking up the claim strings
-    claimNum = claim.substring(1,claim.indexOf('@'));
-    fromEdgeX = claim.substring(claim.indexOf('@')+2,claim.indexOf(','));
-    fromEdgeY = claim.substring(claim.indexOf(',')+1, claim.indexOf(':'));
-    distX = claim.substring(claim.indexOf(':')+2,claim.indexOf('x'));
-    distY = claim.substring(claim.indexOf('x')+1,claim.length);
+var result = fabric(contents,1000,1000);
+console.log(result);
 
-    console.log(`Claim #: ${claimNum}`);
-    console.log(`   Starting at: ${fromEdgeX} x ${fromEdgeY}`);
-    console.log(`   Dimensions: ${distX}x${distY}`);
-    // go to fromEdgeX by fromEdgeY 
-    // check to see if value exists
-    // if no create a new "square"
-    // if yes increment used and move to next square
-});
+// claims.forEach((claim) => {
+//     //breaking up the claim strings
+//     claimNum = claim.substring(1,claim.indexOf('@'));
+//     fromEdgeX = claim.substring(claim.indexOf('@')+2,claim.indexOf(','));
+//     fromEdgeY = claim.substring(claim.indexOf(',')+1, claim.indexOf(':'));
+//     distX = claim.substring(claim.indexOf(':')+2,claim.indexOf('x'));
+//     distY = claim.substring(claim.indexOf('x')+1,claim.length);
+
+//     console.log(`Claim #: ${claimNum}`);
+//     console.log(`   Starting at: ${fromEdgeX} x ${fromEdgeY}`);
+//     console.log(`   Dimensions: ${distX}x${distY}`);
+//     // go to fromEdgeX by fromEdgeY 
+//     // check to see if value exists
+//     // if no create a new "square"
+//     // if yes increment used and move to next square
+// });
