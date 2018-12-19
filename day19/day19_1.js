79ea026f9d770bb02801b091b42599e544f1e206
bjshay10 <bj.shay@d51schools.org>
2018-12-19 15:38:36 -0700

    day19

---
+++ day19/day19_1.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+var contents = fs.readFileSync('input_test.txt', 'utf-8').split('\r\n');
+// var contents = fs.readFileSync('input.txt', 'utf-8').split('\r\n');
+
+//register [ip, input1, input2, input3, output]
+var register = [[0,0,0,0,0,0]];
+var ip, i;
+
+//registers
+var r0,r1,r2,r3,r4,r5 = 0;
+
+//get IP value
+var m = contents[0].toString().split(' ');
+ip = parseInt(m[1]);
+// console.log(instructionPointer);
+
+//increase ip to correct instruction pointer
+ip++;
+
+//loop through contents starting at i(instructionPointer)
+for (ip; ip < contents.length; ip++){
+
+    var instruction = contents[ip].toString().split(' ');
+    console.log(instruction);
+
+    //use instruction[0] to get instruction type addi, seti, etc.
+    switch (instruction[0]) {
+        case 'seti': 
+            //set register(ip) to ip value
+            ip = ip + 1;
+            //set register 1 to instruction [1]
+            r1 = instruction[1];
+            ip = instruction[2]+1;
+            r0 = ip;
+            register.push([r0,r1,r2,r3,r4,r5]);
+            break;
+        default: console.log(`default`);
+            
+    } 
+
+}
+
+
